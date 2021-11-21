f4162cc1537e5e82da6d95814dc9da9d3afb3b18
Juliano Marques <juliano.lima.marques@outlook.com>
2021-11-21 20:39:19 -0300

    pequenos ajustes no parâmetro da função e mudança de nome de variável

--- Ex01.py
+++ Ex01.py
@@ -5,23 +5,23 @@ julia2 = [9, 16, 6, 8, 3]
 camila2 = [10, 5, 6, 1, 4]
 
 
-def checarCachorros(listaA, ListaB):
+def checarCachorros(listaA, listaB):
     # tirando os gatos da lista de Julia com splice
     limite = len(listaA)
     listaJuliaSemGatos = listaA[1:limite-2]
     print(listaJuliaSemGatos)
     # concatenando a lista de ambas garotas
-    listaTotal = listaJuliaSemGatos + ListaB
+    listaTotal = listaJuliaSemGatos + listaB
     print(listaTotal)
     # adulto ou filhote?
-    for e, i in enumerate(listaTotal):
+    for i, idade in enumerate(listaTotal):
         if i >= 3:
             cachorroIdade = 'adulto'
         else:
             cachorroIdade = 'filhote'
     # um apanhado geral de nossas informações
         print(
-            f"O cachorro número {e+1} é um {cachorroIdade} e tem {i} ano(s) de idade")
+            f"O cachorro número {i+1} é um {cachorroIdade} e tem {idade} ano(s) de idade")
 
 # rodando a função com dois conjuntos de dados diferentes
 
